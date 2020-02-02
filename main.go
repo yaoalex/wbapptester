d6613debe288ce62de88e6239976f26fb138c9e4
yaoalex <alexyao1997@gmail.com>
2020-02-02 00:54:50 -0500

    Keep all handler functions in slice

--- main.go
+++ main.go
@@ -7,56 +7,66 @@ import (
 	"go/token"
 	"log"
 	"os"
-	"reflect"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-func test(t string, a int) {
+func parseFunctions(filepath string) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filepath, nil, parser.ParseComments)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var handlerFuncs []string
+
+	for _, decl := range f.Decls {
+		switch t := decl.(type) {
+		case *ast.FuncDecl:
+			responseWriterParamExists := false
+			requestParamExists := false
+			for _, param := range t.Type.Params.List {
+				switch t2 := param.Type.(type) {
+				case *ast.SelectorExpr:
+					paramName := fmt.Sprint(t2.Sel.Name)
+					if paramName == "ResponseWriter" {
+						responseWriterParamExists = true
+					}
+				case *ast.StarExpr:
+					paramName := fmt.Sprint(t2.X)
+					if paramName == "&{http Request}" {
+						requestParamExists = true
+					}
+				}
+			}
+			if responseWriterParamExists && requestParamExists {
+				handlerFuncs = append(handlerFuncs, fmt.Sprint(t.Name))
+			}
+		}
+	}
+	fmt.Println(handlerFuncs)
 }
 
 func main() {
-	app := kingpin.New("webapptester", "Generate tests for your HTTP handler functions")
+	app := kingpin.New("webapptester", "Generate boilerplate code to test your HTTP handlers")
 	file := app.Arg("file", "Go file you would like to create tests for.").Required().String()
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fullpath := dir + "\\" + *file
+	filepath := dir + "\\" + *file
 
-	if _, err := os.Stat(fullpath); err == nil {
-		fmt.Printf("Would create tests for the file: %s\n", fullpath)
-		fset := token.NewFileSet()
-		f, err := parser.ParseFile(fset, fullpath, nil, parser.ParseComments)
-		// var typeNameBuf bytes.Buffer
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, decl := range f.Decls {
-			switch t := decl.(type) {
-			case *ast.FuncDecl:
-				fmt.Printf("Function name: %v\n", t.Name)
-				for _, param := range t.Type.Params.List {
-					switch t2 := param.Type.(type) {
-					case *ast.SelectorExpr:
-						fmt.Println(t2.Sel)
-					case *ast.StarExpr:
-						fmt.Println(t2.X)
-					default:
-						fmt.Println(reflect.TypeOf(t2))
-					}
-				}
-			}
-		}
+	if _, err := os.Stat(filepath); err == nil {
+		fmt.Printf("Creating tests for the file at: %s\n", filepath)
+		parseFunctions(filepath)
 
 	} else if os.IsNotExist(err) {
-		fmt.Printf("Could not find the file; %s\n", fullpath)
+		fmt.Printf("Could not find the file at: %s\n", filepath)
 
 	} else {
-		fmt.Printf("Error looking for the file; %s\n", fullpath)
+		fmt.Printf("Error looking for the file at: %s\n", filepath)
 	}
 
 }
