3446981270047fcb37291676b2c74bac7d313fc4
yaoalex <alexyao1997@gmail.com>
2020-02-04 01:21:37 -0500

    restructured main file

--- webapptester.go
+++ webapptester.go
@@ -10,12 +10,6 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-func buildTestFileName(file string) string {
-	extension := filepath.Ext(file)
-	testFileName := file[0:len(file)-len(extension)] + "_test.go"
-	return testFileName
-}
-
 func main() {
 	app := kingpin.New("webapptester", "Generate boilerplate code to test your HTTP handlers")
 	file := app.Arg("file", "Go file you would like to create tests for.").Required().String()
@@ -27,37 +21,7 @@ func main() {
 	filePath := dir + "\\" + *file
 
 	if _, err := os.Stat(filePath); err == nil {
-		funcInfos, packageName := parseFunctions(filePath)
-		if len(*funcInfos) > 0 {
-			fmt.Println("Creating tests for the following http handlers:")
-			for i, v := range *funcInfos {
-				fmt.Printf("%d. %s\n", i+1, v.Name)
-			}
-			testFileName := buildTestFileName(*file)
-			_, err := os.Stat(testFileName)
-			for err == nil {
-				fmt.Printf("File already exists at the location: %s\n", testFileName)
-				fmt.Println("Please enter a new location to store generated test file")
-				fmt.Scanln(&testFileName)
-				if _, err := os.Stat(testFileName); os.IsNotExist(err) {
-					break
-				}
-			}
-			err = generateTestFile(packageName, testFileName, funcInfos)
-			if err != nil {
-				fmt.Println("Error trying to create the test file")
-				log.Fatal(err)
-			}
-			cmd := exec.Command("gofmt", "-w", testFileName)
-			if _, err := cmd.CombinedOutput(); err != nil {
-				fmt.Println("Failed to run gofmt on the test file")
-				log.Fatal(err)
-			}
-			fmt.Printf("Successfully generate test code at: %s\n", testFileName)
-
-		} else {
-			fmt.Println("Could not find any http handler functions in the file")
-		}
+		parseFile(filePath, *file)
 	} else if os.IsNotExist(err) {
 		fmt.Printf("Could not find the file at: %s\n", filePath)
 
@@ -65,5 +29,44 @@ func main() {
 		fmt.Printf("Error looking for the file at: %s\n", filePath)
 		log.Fatal(err)
 	}
+}
 
+func buildTestFileName(file string) string {
+	extension := filepath.Ext(file)
+	testFileName := file[0:len(file)-len(extension)] + "_test.go"
+	return testFileName
+}
+
+func parseFile(filePath, file string) {
+	funcInfos, packageName := parseFunctions(filePath)
+	if len(*funcInfos) > 0 {
+		fmt.Println("Creating tests for the following http handlers:")
+		for i, v := range *funcInfos {
+			fmt.Printf("%d. %s\n", i+1, v.Name)
+		}
+		testFileName := buildTestFileName(file)
+		_, err := os.Stat(testFileName)
+		for err == nil {
+			fmt.Printf("File already exists at the location: %s\n", testFileName)
+			fmt.Println("Please enter a new location to store generated test file")
+			fmt.Scanln(&testFileName)
+			if _, err := os.Stat(testFileName); os.IsNotExist(err) {
+				break
+			}
+		}
+		err = generateTestFile(packageName, testFileName, funcInfos)
+		if err != nil {
+			fmt.Println("Error trying to create the test file")
+			log.Fatal(err)
+		}
+		cmd := exec.Command("gofmt", "-w", testFileName)
+		if _, err := cmd.CombinedOutput(); err != nil {
+			fmt.Println("Failed to run gofmt on the test file")
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully generate test code at: %s\n", testFileName)
+
+	} else {
+		fmt.Println("Could not find any http handler functions in the file")
+	}
 }
